2df95b834478af66eae4af11131dbf993cf575c1
Alexander Zaharakis <azaharakis@outlook.com>
2019-11-29 11:41:25 -0800

    Refactor app fn to Struct that implements a trait

--- src/commands.rs
+++ src/commands.rs
@@ -8,14 +8,14 @@ pub enum KnownCommands {
     Right,
 }
 
-pub struct Commands<T> {
-    pub place: Box<dyn Fn(Point, Direction) -> Option<T>>,
-    pub left: Box<dyn FnMut(&mut T)>,
-    pub right: Box<dyn FnMut(&mut T)>,
-    pub perform_move: Box<dyn FnMut(&mut T)>,
+pub trait Commands<T> {
+    fn place(&self, p: Point, d: Direction) -> Option<T>;
+    fn left(&self, o: &mut T);
+    fn right(&self, o: &mut T);
+    fn perform_move(&self, o: &mut T);
 }
 
-pub fn run_commands_against<T>(_app: fn() -> Commands<T>) {
+pub fn run_commands_against<T>(app: impl Commands<T>) {
     let commands = vec![
         KnownCommands::Place(point::Point { x: 1, y: 1 }, Direction::North),
         KnownCommands::Left,
@@ -23,21 +23,20 @@ pub fn run_commands_against<T>(_app: fn() -> Commands<T>) {
         KnownCommands::Right,
         KnownCommands::Move,
     ];
-    let mut app = _app();
     let mut object: Option<T> = None;
 
     commands
         .into_iter()
         .map(|command| match command {
             KnownCommands::Place(p, d) => {
-                object = (app.place)(p, d);
+                object = app.place(p, d);
             }
             c @ KnownCommands::Move | c @ KnownCommands::Left | c @ KnownCommands::Right => {
                 match object.as_mut() {
                     Some(o) => match c {
-                        KnownCommands::Move => (app.perform_move)(o),
-                        KnownCommands::Left => (app.left)(o),
-                        KnownCommands::Right => (app.right)(o),
+                        KnownCommands::Move => app.perform_move(o),
+                        KnownCommands::Left => app.left(o),
+                        KnownCommands::Right => app.right(o),
                         _ => {}
                     },
                     _ => {}
--- src/main.rs
+++ src/main.rs
@@ -15,43 +15,49 @@ pub enum Direction {
     West,
 }
 
-fn app() -> Commands<Robot> {
-    Commands {
-        place: Box::new(|p, d| {
-            let board = Board::new(5, 5);
-            if board.is_valid_position(&p) {
-                return Some(Robot::new(p, d));
-            }
-            return None;
-        }),
-        perform_move: Box::new(|robot| {
-            let board = Board::new(5, 5);
-            let potential_pos = Movement::move_a_point_in_direction(
-                robot.get_facing_direction(),
-                robot.get_position(),
-            );
-            if board.is_valid_position(&potential_pos) {
-                robot.set_position(potential_pos);
-                println!("Moved the robot to {:?}", robot.get_position())
-            } else {
-                println!("Moving to {:?} would be invalid", &potential_pos)
-            }
-        }),
-        left: Box::new(|robot| {
-            robot.set_facing_direction(Movement::get_direction_to_left(
-                robot.get_facing_direction(),
-            ));
-            println!("Moved the robot to {:?}", robot.get_facing_direction())
-        }),
-        right: Box::new(|robot| {
-            robot.set_facing_direction(Movement::get_direction_to_right(
-                robot.get_facing_direction(),
-            ));
-            println!("Moved the robot to {:?}", robot.get_facing_direction())
-        }),
+struct App {
+    board: Board,
+}
+
+impl App {
+    fn new() -> App {
+        App {
+            board: Board::new(5, 5),
+        }
+    }
+}
+
+impl Commands<Robot> for App {
+    fn place(&self, p: point::Point, d: Direction) -> Option<Robot> {
+        if self.board.is_valid_position(&p) {
+            return Some(Robot::new(p, d));
+        }
+        return None;
+    }
+    fn left(&self, robot: &mut Robot) {
+        robot.set_facing_direction(Movement::get_direction_to_left(
+            robot.get_facing_direction(),
+        ));
+        println!("Moved the robot to {:?}", robot.get_facing_direction())
+    }
+    fn right(&self, robot: &mut Robot) {
+        robot.set_facing_direction(Movement::get_direction_to_right(
+            robot.get_facing_direction(),
+        ));
+        println!("Moved the robot to {:?}", robot.get_facing_direction())
+    }
+    fn perform_move(&self, robot: &mut Robot) {
+        let potential_pos =
+            Movement::move_a_point_in_direction(robot.get_facing_direction(), robot.get_position());
+        if self.board.is_valid_position(&potential_pos) {
+            robot.set_position(potential_pos);
+            println!("Moved the robot to {:?}", robot.get_position())
+        } else {
+            println!("Moving to {:?} would be invalid", &potential_pos)
+        }
     }
 }
 
 fn main() {
-    run_commands_against(app)
+    run_commands_against(App::new())
 }
